ed6ee9ea8b7c8bb2c63df657fdecbce4d24d0fe1
Ikaika <67708774+IkaikaL@users.noreply.github.com>
2021-03-29 20:00:43 -0500

    updated models.py to include classes

--- cooking_website/display/models.py
+++ cooking_website/display/models.py
@@ -5,8 +5,6 @@ from .PriceFilter import PriceFilter
 from .NameFilter import NameFilter
 from .IngredientFilter import IngredientFilter
 
-# models.py includes classes and forms
-
 price_range = (
     ('under_5','Under $5'),
     ('5_10','$5-$10'),
@@ -27,50 +25,108 @@ meals = (
     ('Lunch','Lunch'),
     ('Dinner','Dinner'),
 )
-
-#class Filter:
-
-#    def __init__(self, key):
-
-#        self._key = key
-        #key is the keyword being used to search
-
-#    def showKey(self):
-#        print(self._key)
-        
-    #def delKey(self):
-
-    #    self._key = 'none'
-
-    #def changeKey(self, key):
-
-    #    self._key = key
         
 class MealPlanForm (forms.Form):
     name = forms.CharField(label="Meal Name")
     day = forms.CharField(label='Day', widget=forms.Select(choices= days))
     meal = forms.CharField(label='Meal', widget=forms.Select(choices= meals))
 
-
-# Developed by Khanh Vu
-# A form where user can look up recipe by selecting price_filter from a dropdown menu and 
-# inputting name and ingredients in textboxes 
-# None of the fields are required in this form.
 class SearchToolForm (forms.Form):
     price_filter = forms.CharField(widget=forms.Select(choices=price_range), required=False)
     name = forms.CharField(label="Name", required=False)
     ingredients = forms.CharField(label="Ingredients",widget=forms.Textarea(), required=False)
 
-
-# Developed by Khanh Vu
-# A form where user can submit a recipe. 
-# All fields, including cost, name, ingredients, direction are required in this form.
 class RecipeSubmissionForm (forms.Form):
     #cost = forms.CharField(widget=forms.Select(choices=price_range))
     cost = forms.CharField(label="Cost")
     name = forms.CharField(label="Name")
     ingredients = forms.CharField(label="Ingredients",widget=forms.Textarea())
     direction = forms.CharField(label="Direction",widget=forms.Textarea())
+
+#developed by Jacob Dickson
+#adds the meal to the correct spot in the MealPlanTemplate.txt
+#name = string, name of meal
+#day = string, day of the week
+#meal = string, type of meal ex: Breakfast, Lunch, and Dinner
+class MealPlanModification():
+    @staticmethod
+    def AddToMealPlan(name, day, meal):
+        comp = day + ":\n"
+        compMeal = "-" + meal + "\n"
+        mealPlan = open("display/MealPlanTemplate.txt", "r")
+        fileContent = mealPlan.readlines()
+        correctDay = False
+        correctMeal = False
+    
+        i = 0
+        while i < len(fileContent) - 1:
+            if fileContent[i] == comp:
+                correctDay = True
+                correctMeal = False
+        
+            if fileContent[i] == compMeal:
+                correctMeal = True
+
+            if correctDay and correctMeal:
+                fileContent[i+1] = name + "\n"
+                correctDay = False
+                correctMeal = False
+            i += 1
+
+        mealPlan.close()
+    
+        mealPlan = open("display/MealPlanTemplate.txt", "w")
+        mealPlan.writelines(fileContent)
+        mealPlan.close()
+
+class FindRecipeDetailsForOneRecipe ():
+    @staticmethod
+    def FindSingleDetails(recipe):
+        with open('display/DataBase.txt') as f:
+            for line in f:
+            
+                if recipe in line:
+                    ingredients = next(f).strip('& ')
+                    cost = next(f).strip('$ ')
+                    instructions = next(f).strip(': ')
+                    return [recipe,cost.strip('\n'),ingredients.strip('\n'),instructions.strip('\n')]
+
+#Developed by Ikaika Lee
+#Class to find the details of the returned recipes based on the user's input
+class FindRecipeDetails():
+    @staticmethod
+    def FindRecipe(recipes):
+        i = 0
+        read = open("display/DataBase.txt", "r")
+        for line in read:
+            if '*' in line:
+                if line.strip('* ').strip('\n') in recipes[i] and i < 5: #checks to see if the current line in the database matches a recipe in the list and makes sure it is only checking the top 3 results
+                    outputDetails = open('display/Output.txt', 'a')
+                    outputDetails.write(line.strip('* ')) #strip the key symbol off the recipe's name in the database
+                    outputDetails.write(next(read).strip('& ')) #strip the key symbol off the recipe's ingredients in the database
+                    outputDetails.write(next(read).strip('$ ')) #strip the key symbol off the recipe's price in the database
+                    outputDetails.write(next(read).strip(': ')) #strip the key symbol off the recipe's steps in the database
+                    outputDetails.write('\n')
+                    outputDetails.close()
+                    read.seek(0,0) #reposition position in database to the top
+                    i += 1 #incrimenting as a recipe is found
+        read.close()
+
+#Developed by Ikaika Lee
+#Class to add submitted recipes to the database
+class RecipeSubmissionOutput():
+    @staticmethod
+    def writeInFile(cost, name, ingredients, direction):
+        recipeSubmit = open("display/DataBase.txt", "a")
+        recipeSubmit.write('\n* ')
+        recipeSubmit.write(name)
+        recipeSubmit.write('\n& ')
+        recipeSubmit.write(ingredients)
+        recipeSubmit.write("\n$ ")
+        recipeSubmit.write(cost)
+        recipeSubmit.write("\n: ")
+        recipeSubmit.write(direction)
+        recipeSubmit.close()
 
 #developed by Anthony Vuong
 class SearchEngine():
